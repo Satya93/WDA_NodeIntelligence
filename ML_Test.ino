9a40a5ed58a8918d1bebb7260e968ed42c87b437
Satyavrat Wagle <satya.wagle@gmail.com>
2017-11-30 16:07:30 -0800

    Refactoring Node Code for Interactive UI

--- ML_Test.ino
+++ ML_Test.ino
@@ -7,30 +7,43 @@
 #include "Time.h"
 
 void setup() {
-  noInterrupts();
-  float cie = 0.05;
-  pd_rgb_led_init();
+    pd_rgb_led_init();
   pd_rgb_led(PD_OFF);
   SerialUSB.begin(9600);
   while(!SerialUSB){
   }
-  //delay(1000);
-  Ml ml;
-  ml.clear_all();
-  //ml.append(200);
-  //ml.append(100);
-  //ml.append(300);
-  ml.sample(100,75);
-  SerialUSB.println("Begin");
-  pd_rgb_led(PD_RED);
-  ml.regression(cie,0.1);
-  pd_rgb_led(PD_OFF);
-  SerialUSB.println("Done");
-
+  SerialUSB.print("Enter Threshold CIE : ");
 }
-//------------------------------------------------------------------------------
-// WARNING idle loop has a very small stack (configMINIMAL_STACK_SIZE)
-// loop must never block
 void loop() {
-  // Not used.
+  float cie = 0.00005;
+  float inpu;
+  float rate;
+  byte incomingByte = 0;
+  int flg = 0;
+  
+  if(SerialUSB.available() > 0) {
+    incomingByte = SerialUSB.read();
+    inpu = (int)incomingByte-48;
+    rate = inpu/10;
+    SerialUSB.print("Rate set to : ");
+    SerialUSB.println(rate);
+    flg = 1;
+  }
+  if(flg==1){
+    //noInterrupts();
+    //delay(1000);
+    Ml ml;
+    ml.clear_all();
+    //ml.append(200);
+    //ml.append(100);
+    //ml.append(300);
+    ml.sample(100,75);
+    SerialUSB.println("Begin");
+    pd_rgb_led(PD_RED);
+    ml.regression(cie,rate);
+    pd_rgb_led(PD_OFF);
+    SerialUSB.println("Done");
+    //interrupts();
+  }
+
 }
